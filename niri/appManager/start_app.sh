0b1a42daaab9c23973894626bf3221d3b0b72b24
朽朽朽朽朽 <694253220@qq.com>
2025-10-21 01:41:36 +0800

    feat: 或许优化了自启动脚本的逻辑

--- niri/appManager/start_app.sh
+++ niri/appManager/start_app.sh
@@ -8,27 +8,41 @@ CUR_MON=$(niri msg --json focused-output | jq -r .name)
 partial_contains() {
     local str1="$1"
     local str2="$2"
-    [[ "$str1" == *"$str2"* ]]
+    [[ "$str1" == *"$str2"* ]] || [[ "$str2" == *"$str1"* ]]
 }
 
+mkdir -p "/tmp/start_app/"
+APP="/tmp/start_app/app"
+BLOCK="/tmp/start_app/block"
+EXIT_CODE="/tmp/start_app/exit"
+
+echo "" > $APP
+echo "" > $BLOCK
+echo 0 > $EXIT_CODE
+
 event_monitor(){
-    app=${1,,}
-    block=${2,,}
+    echo "Starting"
     # 后台监听 niri 事件流
     niri msg --json event-stream | while IFS= read -r line; do
         # 获取事件名（JSON 的第一个 key）
         event_name=$(echo "$line" | jq -r 'keys[0] // empty')
         case "$event_name" in
                 "WindowOpenedOrChanged")
-                    echo "$line"
+                    app=$(< $APP)
+                    block=$(< $BLOCK)
+#                     echo "$line"
                     title=$(echo "$line" | jq -r '.WindowOpenedOrChanged.window.title')
                     app_id=$(echo "$line" | jq -r '.WindowOpenedOrChanged.window.app_id')
-                    echo "monit app => $title"
                     echo "check block $block and $title"
-                    if [[ "$block" != "${title,,}" ]]; then
-                       if partial_contains "$app" "${app_id,,}"; then
-                            echo "匹配到app=$app_id title=$title block=$block"
-                            break
+                    if [[ "${block,,}" != "${title,,}" ]]; then
+                        echo "check app == $app"
+                       if [[ -n "$app" ]]; then
+                            echo "monit app => $app and $app_id"
+                            if partial_contains "${app,,}" "${app_id,,}"; then
+                               echo "匹配到app=$app_id title=$title block=$block"
+#                             break
+                                echo 1 > $EXIT_CODE
+                            fi
                         fi
                     fi
                 ;;
@@ -40,7 +54,9 @@ event_monitor(){
     done
 }
 
-sleep 1
+event_monitor &
+
+sleep 5
 
 while IFS= read -r line; do
 
@@ -63,7 +79,15 @@ while IFS= read -r line; do
 
     nohup gtk-launch $app  >/dev/null 2>&1 &
 
-    event_monitor "$app" "$block"
+    exit_code=$(< $EXIT_CODE)
+    while [[ $exit_code == 0 ]]; do
+        exit_code=$(< $EXIT_CODE)
+        echo $app > $APP
+        echo $block > $BLOCK
+#         echo "set data ==> $APP"
+    done
+
+    echo 0 > $EXIT_CODE
 
      if [[ -n $monitor ]]; then
         niri msg action move-window-to-monitor $monitor
@@ -75,4 +99,10 @@ while IFS= read -r line; do
 
 done < $APPLIST
 
+echo "done"
+
 niri msg action focus-monitor $CUR_MON
+
+rm -rf "/tmp/start_app/"
+
+kill -- -$$
